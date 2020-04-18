c52cd21b3cacf54f990b3c0582f4e930391bdcc1
Czeksu <camiloczekaj@gmail.com>
2020-04-18 15:58:46 +0200

    Updated DataTables

--- js/countries.js
+++ js/countries.js
@@ -3,37 +3,48 @@ var app = new Vue({
   data: {
     countries: []
     },
-      mounted () {
-          fetch("https://api.covid19api.com/summary")
-          .then(response => response.json())
-          .then((data) =>{
-              this.countries = data;
-          })
+    methods: {
+ 
     },
-template:`
-<div>
-    <h1>Covid-19 APP</h1><br>
-    <table id="tab" class="table table-dark">
-        <thead align="center">
-            <th scope="col">Kraj</th>
-            <th scope="col">Łącznie zakażeń</th>
-            <th scope="col">Nowe zakażenia</th>
-            <th scope="col">Łącznie zgonów</th>
-            <th scope="col">Nowe zgony</th>
-            <th scope="col">Łącznie wyzdrowień</th>
-            <th scope="col">Nowe wyzdrowienia</th>
-        </thead>
-        <tbody align="center">
-        <tr style="border: 1px solid black;" v-for="(country, index in countries.Countries">
-            <td>{{countries.Countries[index].Country}}</td>
-            <td>{{countries.Countries[index].TotalConfirmed}}</td>
-            <td>{{countries.Countries[index].NewConfirmed}}</td>
-            <td>{{countries.Countries[index].TotalDeaths}}</td>
-            <td>{{countries.Countries[index].NewDeaths}}</td>
-            <td>{{countries.Countries[index].TotalRecovered}}</td>
-            <td>{{countries.Countries[index].NewRecovered}}</td>
-        </tr>
-        </tbody>
-</div>
-`,
-});
+    mounted () {
+        fetch("https://api.covid19api.com/summary")
+        .then(response => response.json())
+        .then((data) =>{
+            this.countries = data;
+        })
+        $(document).ready(function(){
+            $('#tab').dataTable({
+                "order": [[ 1, "asc" ]],
+            });
+        });
+    },
+    template:`
+    <div>
+        <h1>Covid-19 APP</h1><br>
+        <table id="tab" class="display">
+            <thead>
+                <tr>
+                    <th>Kraj</th>
+                    <th>Łącznie zakażeń</th>
+                    <th>Nowe zakażenia</th>
+                    <th>Łącznie zgonów</th>
+                    <th>Nowe zgony</th>
+                    <th>Łącznie wyzdrowień</th>
+                    <th>Nowe wyzdrowienia</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(country, index in countries.Countries">
+                    <td>{{countries.Countries[index].Country}}</td>
+                    <td>{{countries.Countries[index].TotalConfirmed}}</td>
+                    <td>{{countries.Countries[index].NewConfirmed}}</td>
+                    <td>{{countries.Countries[index].TotalDeaths}}</td>
+                    <td>{{countries.Countries[index].NewDeaths}}</td>
+                    <td>{{countries.Countries[index].TotalRecovered}}</td>
+                    <td>{{countries.Countries[index].NewRecovered}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+    });
